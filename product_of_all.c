b61aaf5fbc96683b0959a1f290901c3366261090
Nelson Morales <nelsonenr@proton.me>
2023-09-09 15:25:29 -0400

    Refactor and comment algorithm

--- product_of_all.c
+++ product_of_all.c
@@ -13,20 +13,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void print_arr(register int *ptr, register unsigned size)
+{
+	/* assert(size != 0); */
+	while (--size > 0)
+		printf("%d ", *ptr++);
+	printf("%d\n", *ptr);
+}
+
+/*
+ * The array of integers `arr' is defined as
+ * 	arr = [a_1, a_2, ..., a_(n-1), a_n]
+ * for n >= 2. Let s and be array such that
+ *
+ * s = [1, a_1, a_1a_2, ..., a_1a_2...a_(n-1)]
+ * t = [a_2a_3...a_n, ..., a_(n-1)a_n, a_n, 1]
+ * 
+ * new_arr[i] = s[i] * t[i]
+ */
+
 int *foo(const int arr[], unsigned size)
 {
-	int i, acc = 1;
+	int i, acc;
 	int *new_arr = calloc(size, sizeof(int));
 
 	for (i = 0; i < size; ++i)
 		new_arr[i] = 1;
 
-	for (i = 1; i < size; ++i)
-		new_arr[i] = new_arr[i - 1] * arr[i - 1];
+	acc = 1;
+	for (i = 1; i < size; ++i) {
+		acc *= arr[i - 1];
+		new_arr[i] *= acc;
+	}
 
+	acc = 1;
 	for (i = size - 2; i >= 0; --i) {
-		acc = acc * arr[i + 1];
-		new_arr[i] = new_arr[i] * acc;
+		acc *= arr[i + 1];
+		new_arr[i] *= acc;
 	}
 
 	return new_arr;
@@ -34,13 +57,10 @@ int *foo(const int arr[], unsigned size)
 
 int main(void)
 {
-	int i, arr[] = {1, 2, 3, 4, 5};
+	int arr[] = {1, 2, 3, 4, 5};
 	unsigned size = sizeof(arr) / sizeof(int);
 	int *new_arr = foo(arr, size);
 
-	for (i = 0; i < size - 1; ++i)
-		printf("%d ", new_arr[i]);
-	printf("%d\n", new_arr[size - 1]);
-
+	print_arr(new_arr, size);
 	exit(0);
 }
