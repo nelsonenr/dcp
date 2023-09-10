6dd286ca226beb13936b587ca33d93b9059a6a84
Nelson Morales <nelsonenr@proton.me>
2023-09-10 19:15:54 -0400

    Create serialize.py

---
+++ serialize.py
@@ -0,0 +1,64 @@
+"""
+See LICENSE file for copyright and license details.
+
+Given the root to a binary tree, implement serialize(root), which
+serializes the tree into a string, and deserialize(s), which
+deserializes the string back into the tree.
+
+For example, given the following Node class
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+The following test should pass:
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+"""
+from collections import deque
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def bar(queue: deque) -> Node:
+    val = queue.popleft()
+    if val == '\t':
+        return None
+    else:
+        node = Node(val)
+        node.left = bar(queue)
+        node.right = bar(queue)
+        return node
+
+
+def deserialize(code: str) -> Node:
+    l = code.split('/')
+    return bar(deque(l))
+
+
+def foo(node: Node, queue: deque):
+    if node is None:        
+        queue.append('\t')
+    else:
+        queue.append(node.val)
+        foo(node.left, queue)
+        foo(node.right, queue)
+
+
+def serialize(root: Node) -> str:
+    queue = deque([])
+    foo(root, queue)
+    return '/'.join(queue)
+
+
+if __name__=='__main__':
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
